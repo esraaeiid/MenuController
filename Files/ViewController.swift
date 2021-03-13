3158cb76f8a5bf6ea657e5bbf2337b088e1f0698
Esraa Eid <esraa@ryets.com>
2021-03-13 13:29:33 -0800

    menu controller

--- Files/ViewController.swift
+++ Files/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+  
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        let button = UIButton(primaryAction: UIAction(title: "Menu Button", handler: { (action) in
+            //handle action
+            
+        }))
+        
+        button.menu = UIMenu(title: "Menu button 1", children: [UIMenu(title: "", options: UIMenu.Options.displayInline, children: (1...4).map{
+            UIAction(title: "Selected Item \($0)") { (action) in
+                 print("tapped item \(action)")
+            }})])
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.showsMenuAsPrimaryAction = true
+        
+        view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
     }
+    
+
+    
 
 
 }
